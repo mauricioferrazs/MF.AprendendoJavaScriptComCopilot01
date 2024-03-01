1a4298be11c94d4b74d8f9f49c7a1522b7bff827
Mauricio Antonio Chuahy Ferraz Silva <mauricio.ferrazs@outlook.com>
2024-03-01 00:15:48 -0300

    Adicionado Exemplo0001

---
+++ js/Exemplo0001.js
@@ -0,0 +1,8 @@
+// Este é um exemplo básico de "Hello world!" em JavaScript.
+// O código abaixo exibe uma mensagem no console do navegador.
+
+// Declaramos uma variável chamada "mensagem" e atribuímos a ela o valor "Olá, mundo!".
+const mensagem = "Olá, mundo!";
+
+// Usamos a função "console.log()" para imprimir a mensagem no console.
+console.log(mensagem);
